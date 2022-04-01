4d998d88573d59206101b1db228761fd405709bd
Fayaz Yusuf Khan <fayaz.yusuf.khan@gmail.com>
2022-04-01 22:18:15 +0530

    Fix roll type

--- trinket-roll.py
+++ trinket-roll.py
@@ -1,7 +1,7 @@
 embed
 <drac2>
 trinket_table = get_gvar("203cc488-ee1c-4d75-8bd5-01c6f3bbcbd5")
-trinket_dict = dict(line.strip().split("\t") for line in trinket_table.strip().splitlines())
-trinket = trinket_dict[roll("1d100")]
+trinket_dict = [line.strip() for line in trinket_table.strip().splitlines()]
+trinket = trinket_dict[roll("1d100") - 1]
 </drac2>
 -title "You found {{trinket}}"
