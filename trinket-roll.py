d0e9a948f2836811a07ae2d35fddea8475f529f5
Fayaz Yusuf Khan <fayaz.yusuf.khan@gmail.com>
2022-04-03 10:40:14 +0530

    Improve output format

--- trinket-roll.py
+++ trinket-roll.py
@@ -1,7 +1,11 @@
 embed
 <drac2>
 trinket_table = get_gvar("203cc488-ee1c-4d75-8bd5-01c6f3bbcbd5")
-trinket_dict = [line.strip() for line in trinket_table.strip().splitlines()]
-trinket = trinket_dict[roll("1d100") - 1]
+trinkets = [line.strip() for line in trinket_table.strip().splitlines()]
+rolled = roll("1d100")
+trinket = trinkets[rolled - 1]
 </drac2>
--title "You found {{trinket}}"
+-title "Trinket hunt!"
+-desc "You go out looking for trinkets and find `{{trinket}}`".
+-f "Your roll|`{{rolled}}`|inline"
+-footer "Trinket table | PHB 160-161"
